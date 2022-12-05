51df40bf0bf29fa5b7ad1e2a5395c60ab9c3eaf2
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2022-12-05 21:11:01 +0700

    apply react framework

--- webpack.config.js
+++ webpack.config.js
@@ -6,59 +6,72 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
 };
 
 const common = {
-  devtool: 'source-map',
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-        include: PATHS.app,
-      }
-    ],
-  },
-  plugins: [
-    new ESLintPlugin(),
-    new StylelintPlugin(),
-  ],
+	devtool: 'source-map',
+	entry: {
+		app: PATHS.app
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+				include: PATHS.app,
+			},
+			{
+				test: /\.jsx?$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+					}
+				},
+				include: PATHS.app
+			}
+		],
+	},
+	plugins: [
+		new ESLintPlugin(),
+		new StylelintPlugin(),
+	],
 }
 
 // Default configuration
 if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {
-    mode: 'production',
-    stats: 'errors-only',
-    devServer: {
-      static: PATHS.build,
-      historyApiFallback: true,
-      hot: true,
-      compress: true,
-      host: process.env.HOST,
-      port: process.env.PORT,
-      client: {
-        progress: true,
-      },
-    },
-  });
+	module.exports = merge(common, {
+		mode: 'production',
+		stats: 'errors-only',
+		devServer: {
+			static: PATHS.build,
+			historyApiFallback: true,
+			hot: true,
+			compress: true,
+			host: process.env.HOST,
+			port: process.env.PORT,
+		},
+		performance: {
+			hints: false,
+		}
+	});
 }
 
 if (TARGET === 'build') {
-  module.exports = merge(common, {
-    mode: 'development',
-  });
+	module.exports = merge(common, {
+		mode: 'development',
+	});
 }
 
