5e2cd29be8a006c898b483cc1263cd1209fa6aa3
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2022-12-06 21:03:01 +0700

    Updates

---
+++ bundle.js
@@ -0,0 +1,1 @@
+(()=>{var e,r,t={495:e=>{e.exports=function(){var e=document.createElement("h1");return e.innerHTML="Hello world",e}}},n={};e=function e(r){var o=n[r];if(void 0!==o)return o.exports;var d=n[r]={exports:{}};return t[r](d,d.exports,e),d.exports}(495),r=document.createElement("div"),document.body.appendChild(r),r.appendChild(e())})();
