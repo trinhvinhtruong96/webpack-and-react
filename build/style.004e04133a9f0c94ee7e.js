35204f3889624f9316a77ab1dd41b636eb1bbc8b
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2022-12-08 08:49:31 +0700

    Improving Caching Behavior

---
+++ build/style.004e04133a9f0c94ee7e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebpack_and_react=self.webpackChunkwebpack_and_react||[]).push([[296],{5803:()=>{}},a=>{var e;e=5803,a(a.s=e)}]);
--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,15 @@ const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
 	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+	build: path.join(__dirname, 'build'),
+	style: path.join(__dirname, 'app/main.css')
 };
 
 const common = {
 	devtool: 'source-map',
 	entry: {
-		app: PATHS.app
+		app: PATHS.app,
+		style: PATHS.style
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx']
